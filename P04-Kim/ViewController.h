a0062c69d3ea9fa6152c2b565a4a8946f91708bf
ETS Admin <etsadmin@ETSMacbook1.local>
2017-03-13 11:28:46 -0400

    first push

---
+++ P04-Kim/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  P04-Kim
+//
+//  Created by ETS Admin on 3/13/17.
+//  Copyright © 2017 Joe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ P04-Kim/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  P04-Kim
+//
+//  Created by ETS Admin on 3/13/17.
+//  Copyright © 2017 Joe. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
